0ae40c8486fb5392624612eb04d1d588decaac51
Stéphane Krattinger <stephane.krattinger@mobi.ch>
2021-05-05 10:27:11 +0200

    java 11 version

--- module1/src/test/java/ch/mobi/test/Module1ClassTest.java
+++ module1/src/test/java/ch/mobi/test/Module1ClassTest.java
@@ -9,8 +9,8 @@ class Module1ClassTest {
 
     @Test
     void should_contains_correct_child() {
-        Module2Class mockedChild = mock(Module2Class.class);
-        Module1Class module = new Module1Class(mockedChild);
+        var mockedChild = mock(Module2Class.class);
+        var module = new Module1Class(mockedChild);
         assertSame(mockedChild, module.getChild());
     }
 }
